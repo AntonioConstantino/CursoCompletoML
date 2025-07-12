69965965675f61857a9ce54670507635f9f1e061
AntonioConstantino <97559196+AntonioConstantino@users.noreply.github.com>
2025-07-11 22:44:57 -0600

    Add docker compose

---
+++ Contenedores/Docker/hola_mundo.py
@@ -0,0 +1,8 @@
+import numpy as np
+lista_ejemplo=["Pedro", "Bingo","Andrea"]
+for i in lista_ejemplo:
+    print("===================")
+    print(f"Hola Mundo para : {i}\n")
+array=np.array(lista_ejemplo)
+print("______________________________")
+print(f"formato del array :{array}")
